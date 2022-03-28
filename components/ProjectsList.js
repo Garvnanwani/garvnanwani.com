00eeeb76ddd79f6d908496bff3ef3ae653b96210
garv nanwani <garvwashere@gmail.com>
2022-03-28 14:26:20 +0530

    updated project list

--- components/ProjectsList.js
+++ components/ProjectsList.js
@@ -2,45 +2,45 @@ import ProjectCard from './ProjectCard'
 
 const projects = [
   {
-    title: 'project 1',
-    description: 'this is project 1 description',
-    image: '/project-1.jpg',
-    tech: ['django', 'html', 'css']
+    title: 'VoiceClub',
+    description: 'A voice chat app wherein you can create your own room and discuss over topics.',
+    image: '/project-voiceClub.jpg',
+    tech: ['react', 'express', 'webrtc', 'socket.io']
   },
   {
-    title: 'project 2',
-    description: 'this is project 1 description',
-    image: '/project-2.png',
-    tech: ['django', 'html', 'css']
+    title: 'Blooms Bay',
+    description: 'ecommerce app for flowers with an admin panel made with mern stack',
+    image: '/project-bloomsbay.png',
+    tech: ['mongodb', 'express', 'react', 'node']
   },
   {
-    title: 'project 3',
-    description: 'this is project 1 description',
-    image: '/project-3.jpg',
-    tech: ['django', 'html', 'css']
+    title: 'Chase Crypto',
+    description: 'A crypto currency tracker app made with react and coinbase api',
+    image: '/project-chaseCrypto.jpg',
+    tech: ['react', 'coinbase api']
   },
   {
-    title: 'project 4',
-    description: 'this is project 1 description',
-    image: '/project-4.png',
-    tech: ['django', 'html', 'css']
+    title: 'Play Your Taste',
+    description: 'Music Streaming App with shazam api',
+    image: '/project-playTaste.png',
+    tech: ['react', 'react-router-dom']
   }
 ]
 
 const ProjectsList = () => {
   return (
     <div className="flex flex-wrap md:justify-around lg:justify-between">
-      {projects.map((project) => {
+      { projects.map((project) => {
         return (
           <ProjectCard
-            key={project.title}
-            title={project.title}
-            description={project.description}
-            image={project.image}
-            tech={project.tech}
+            key={ project.title }
+            title={ project.title }
+            description={ project.description }
+            image={ project.image }
+            tech={ project.tech }
           />
         )
-      })}
+      }) }
     </div>
   )
 }
