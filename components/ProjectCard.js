57c248f951a5626f46e7aec131f6bd258c8926f7
Garv nanwani <garvwashere@gmail.com>
2022-04-15 21:03:36 +0530

    improved projects page design

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,42 +1,48 @@
 import Image from 'next/image'
 import { FaExternalLinkAlt, FaGithub } from 'react-icons/fa'
 
-const ProjectCard = ({ title, description, image, tech }) => {
+const ProjectCard = ({ title, description, image, tech, githublink, livelink }) => {
   return (
-    <div className="flex flex-row flex-wrap mb-20 md:mr-16 sm:mx-auto md:mx-0">
-      <div>
-        <Image
-          width={350}
-          height={250}
-          src={image}
-          alt="project cover image"
-          className="rounded-lg"
-        />
+    <div className="mb-20 md:mr-16 sm:mx-auto md:mx-0">
+      <div className="lg:flex items-center justify-center w-[90%] mx-auto">
+        <div>
+          <Image
+            width={ 600 }
+            height={ 350 }
+            src={ image }
+            alt="project cover image"
+            className="rounded-lg"
+          />
+        </div>
 
-        <div className="relative px-4 -mt-8 ">
+        <div className="relative lg:right-4 lg:w-[50%]">
           <div className="p-6 bg-white rounded-lg shadow-lg">
             <h4 className="mt-1 text-xl font-semibold leading-tight uppercase truncate">
-              {title}
+              { title }
             </h4>
 
-            <div className="my-2">{description}</div>
+            <div className="my-2">{ description }</div>
             <div className="my-2">Tech Stack</div>
             <div>
-              {tech.map((item) => {
+              { tech.map((item) => {
                 return (
-                  <span key={item} className="mr-2">
-                    {item}
+                  <span key={ item } className="mr-2">
+                    { item }
                   </span>
                 )
-              })}
+              }) }
             </div>
             <div className="flex mt-6">
-              <span className="font-semibold text-teal-600 mr-14 text-md">
-                <FaGithub />
-              </span>
-              <span className="text-sm text-gray-600">
-                <FaExternalLinkAlt />
-              </span>
+              <a href={ githublink }>
+                <span className="font-semibold text-teal-600 mr-14 text-md">
+                  <FaGithub />
+                </span>
+              </a>
+              <a href={ livelink }>
+                <span className="text-sm text-gray-600">
+                  <FaExternalLinkAlt />
+                </span>
+              </a>
             </div>
           </div>
         </div>
--- components/ProjectsList.js
+++ components/ProjectsList.js
@@ -4,32 +4,40 @@ const projects = [
   {
     title: 'VoiceClub',
     description: 'A voice chat app wherein you can create your own room and discuss over topics.',
-    image: '/project-voiceClub.jpg',
-    tech: ['react', 'express', 'webrtc', 'socket.io']
+    image: '/project-voiceClub.png',
+    tech: ['react', 'express', 'webrtc', 'socket.io'],
+    githublink: '',
+    livelink: ''
   },
   {
     title: 'Blooms Bay',
     description: 'ecommerce app for flowers with an admin panel made with mern stack',
     image: '/project-bloomsbay.png',
-    tech: ['mongodb', 'express', 'react', 'node']
+    tech: ['mongodb', 'express', 'react', 'node'],
+    githublink: '',
+    livelink: ''
   },
   {
     title: 'Chase Crypto',
     description: 'A crypto currency tracker app made with react and coinbase api',
-    image: '/project-chaseCrypto.jpg',
-    tech: ['react', 'coinbase api']
+    image: '/project-chaseCrypto.png',
+    tech: ['react', 'coinbase api'],
+    githublink: '',
+    livelink: ''
   },
   {
     title: 'Play Your Taste',
     description: 'Music Streaming App with shazam api',
     image: '/project-playTaste.png',
-    tech: ['react', 'react-router-dom']
+    tech: ['react', 'react-router-dom'],
+    githublink: '',
+    livelink: ''
   }
 ]
 
 const ProjectsList = () => {
   return (
-    <div className="flex flex-wrap md:justify-around lg:justify-between">
+    <div className="">
       { projects.map((project) => {
         return (
           <ProjectCard
@@ -38,6 +46,8 @@ const ProjectsList = () => {
             description={ project.description }
             image={ project.image }
             tech={ project.tech }
+            githublink={ project.githublink }
+            livelink={ project.livelink }
           />
         )
       }) }
